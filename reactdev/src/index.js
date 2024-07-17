c43c11fd94e5c3016cf0a785e3def49f32580bae
mcc9513 <58860627+mcc9513@users.noreply.github.com>
2024-07-17 10:25:24 -0400

    Update index.js

--- reactdev/src/index.js
+++ reactdev/src/index.js
@@ -1,12 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from "react"; //wrapper component that identifies problems by adding additional checks and warnings
+import { createRoot } from "react-dom/client"; //imports function that creates a HTML element 'root' where the app will be rendered
+
 import "./styles.css";
 
-import App from "./App";
+import App from "./App"; //imports app component of app.js file, main component of react app
 
-const root = createRoot(document.getElementById("root"));
-root.render(
+const root = createRoot(document.getElementById("root")); //selects DOM element with HTML id 'root', calls createRoot
+root.render( //renders React components to DOM
   <StrictMode>
-    <App />
+    <App /> //App conponent beind rendered
   </StrictMode>
 );
